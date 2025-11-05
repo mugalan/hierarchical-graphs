41c33f66c63a45a94da6f7a3b86184ceaf59b9f3
Sanjeeva Maithripala <77828283+mugalan@users.noreply.github.com>
2025-11-05 10:38:39 +0530

    lets see

--- hierarchical_graph/__init__.py
+++ hierarchical_graph/__init__.py
@@ -1,6 +1,5 @@
 from .core import HierarchicalGraph
 
-# Lazy-load EPlusUtil to avoid requiring pyenergyplus before bootstrap
 __version__ = "0.1.0"
 __all__ = [ "HierarchicalGraph", "__version__"]
 
